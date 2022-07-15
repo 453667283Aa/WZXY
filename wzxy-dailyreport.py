2c427ac18278e0eb11a6c91cb2168ed254a41e02
453667293H <94662168+453667283Aa@users.noreply.github.com>
2022-07-15 10:04:33 +0800

    Update wzxy-dailyreport.py

--- wzxy-dailyreport.py
+++ wzxy-dailyreport.py
@@ -148,6 +148,7 @@ class WoZaiXiaoYuanPuncher:
             "city": os.environ["WZXY_CITY"],
             "district": os.environ["WZXY_DISTRICT"],
             "province": os.environ["WZXY_PROVINCE"],
+            "township": os.environ["WZXY_TOWNSHIP"],
             "street": os.environ["WZXY_STREET"],
             "myArea": "",
             "areacode": "",
